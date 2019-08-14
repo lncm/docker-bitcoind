4d0bbfe125b2fe3b025a32f078e58d309d7f590f
Damian Mee <spam@meedamian.com>
2019-08-15 06:36:35 +0700

    simpler?

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-wget -N https://github.com/multiarch/qemu-user-static/releases/download/v4.0.0-5/x86_64_qemu-arm-static.tar.gz
-tar -xvf x86_64_qemu-arm-static.tar.gz
-
-docker run --rm --privileged multiarch/qemu-user-static:register
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
